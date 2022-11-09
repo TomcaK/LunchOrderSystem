38b21b43b1bc9431ebcb323501845be5402b3aa7
Tomas <tomcakopulety@seznam.cz>
2022-11-09 19:12:31 +0100

    create table keyword bug resolved

--- src/main/java/cz/comkop/lunchordersystem/model/LunchOrder.java
+++ src/main/java/cz/comkop/lunchordersystem/model/LunchOrder.java
@@ -1,23 +1,26 @@
 package cz.comkop.lunchordersystem.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+import org.hibernate.Hibernate;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.Objects;
 
-@NoArgsConstructor
 @Getter
 @Setter
 @Entity
+@NoArgsConstructor
 @Table(name = "orders")
+
 public class LunchOrder {
     @Id
     @GeneratedValue
     private int id;
-    private String week;
+    private LocalDate fromDate;
+
+    private LocalDate toDate;
     private int monday;
     private int tuesday;
     private int wednesday;
@@ -25,7 +28,7 @@ public class LunchOrder {
     private int friday;
     @OneToOne
     @JsonIgnore
-    @JoinColumn(name = "user_id", nullable = false)
+    @JoinColumn(name = "userId", nullable = false)
     private User user;
 
 
@@ -36,17 +39,32 @@ public class LunchOrder {
         this.thursday = thursday;
         this.friday = friday;
         this.user = user;
-        this.week = getWeek();
+        LocalDate date = LocalDate.now();
+        this.fromDate = createFromDate(date);
+        this.toDate = createToDate(date);
     }
 
-    private String getWeek() {
-        LocalDate date = LocalDate.now();
-        int monday = Math.abs(date.getDayOfWeek().getValue() - 1);
-        int friday = Math.abs(date.getDayOfWeek().getValue() - 5);
-//        LocalDate weekStart = LocalDate.of(date.getYear(), date.getMonthValue(), date.getDayOfMonth() - differenceOfDayFromMonday);
-//    LocalDate weekEnd = LocalDate.of(date.getYear(),date.getMonthValue(),date.getDayOfMonth()+ differenceOfDayToFriday);
-        return null;
+    private LocalDate createFromDate(LocalDate date) {
+        return LocalDate.of(date.getYear(), date.getMonthValue(), date.getDayOfMonth() - (date.getDayOfWeek().getValue() - 1));
     }
+
+    private LocalDate createToDate(LocalDate date) {
+        return LocalDate.of(date.getYear(), date.getMonthValue(), date.getDayOfMonth() + (Math.abs(date.getDayOfWeek().getValue() - 5)));
+    }
+
+
+
+//    public LunchOrder(LocalDate fromDate, LocalDate toDate, int monday, int tuesday, int wednesday, int thursday, int friday, User user) {
+//        this.monday = monday;
+//        this.tuesday = tuesday;
+//        this.wednesday = wednesday;
+//        this.thursday = thursday;
+//        this.friday = friday;
+//        this.user = user;
+//        this.fromDate = fromDate;
+//        this.toDate = toDate;
+//    }
+
 
 //Week of
 //
@@ -57,4 +75,6 @@ public class LunchOrder {
 //        System.out.println(weekEnd);
 //    period = Period.between(weekStart,weekEnd);
 //String week = String.format("%s - %s",weekStart.format(DateTimeFormatter.ofPattern("d.M.y")),weekEnd.format(DateTimeFormatter.ofPattern("d.M.y")));
+
+
 }
